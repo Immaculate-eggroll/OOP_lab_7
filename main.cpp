dde07d145c8dd9ecd7d69f02cf59b608fbb3d5df
Immaculate-eggroll <blessed.potato@yahoo.com>
2025-12-26 18:27:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,7 @@ int main() {
     try {
         Game game;
         
-        std::cout << "=== NPC Battle Simulation ===" << std::endl;
-        std::cout << "Rules:" << std::endl;
-        std::cout << "  - Orcs kill Bears" << std::endl;
-        std::cout << "  - Bears kill Knights" << std::endl;
-        std::cout << "  - Knights kill Orcs" << std::endl;
-        std::cout << "  - Map size: 100x100" << std::endl;
-        std::cout << "  - Initial NPCs: 50" << std::endl;
-        std::cout << "  - Game duration: 30 seconds" << std::endl;
-        std::cout << std::endl;
-        
-        std::cout << "Press Enter to start..." << std::endl;
+        std::cout << "Press Enter to start simulation..." << std::endl;
         std::cin.get();
         
         game.start();
